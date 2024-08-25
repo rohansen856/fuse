ee5c704b6fdb66593e01ffbf20a9fb709d2ff07c
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-25 17:47:12 +0530

    mera teesra commit :))

---
+++ backend/database/databaseConnection.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func InitDatabase() {
+	Client = DBinstance()
+}
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mongoURI := os.Getenv("MONGODB_URL")
+	clientOptions, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	err = clientOptions.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to mongodb")
+	return clientOptions
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("your_database_name").Collection(collectionName)
+	return collection
+}
