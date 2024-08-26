6f287bae1836f25b3e58079ad230df0334d1bc65
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-26 11:23:52 +0530

    frontend changes

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,22 +1,22 @@
-import { redirect } from "next/navigation"
-import { authOptions } from "@/lib/auth"
-import { db } from "@/lib/db"
-import { getCurrentUser } from "@/lib/session"
-import { EmptyPlaceholder } from "@/components/empty-placeholder"
-import { DashboardHeader } from "@/components/header"
-import { PostCreateButton } from "@/components/post-create-button"
-import { PostItem } from "@/components/post-item"
-import { DashboardShell } from "@/components/shell"
+import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
+import { EmptyPlaceholder } from "@/components/empty-placeholder";
+import { DashboardHeader } from "@/components/header";
+import { PostCreateButton } from "@/components/post-create-button";
+import { PostItem } from "@/components/post-item";
+import { DashboardShell } from "@/components/shell";
 
 export const metadata = {
   title: "Dashboard",
-}
+};
 
 export default async function DashboardPage() {
-  const user = await getCurrentUser()
+  const user = await getCurrentUser();
 
   if (!user) {
-    redirect(authOptions?.pages?.signIn || "/login")
+    redirect(authOptions?.pages?.signIn || "/login");
   }
 
   const posts = await db.news.findMany({
@@ -32,7 +32,7 @@ export default async function DashboardPage() {
     orderBy: {
       updatedAt: "desc",
     },
-  })
+  });
 
   return (
     <DashboardShell>
@@ -58,5 +58,5 @@ export default async function DashboardPage() {
         )}
       </div>
     </DashboardShell>
-  )
+  );
 }
