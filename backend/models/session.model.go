e43306d2410dae29949755145866e255e5a048aa
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-26 11:23:30 +0530

    ye modified kyu dikha rha pata nhi

--- backend/models/session.model.go
+++ backend/models/session.model.go
@@ -8,7 +8,7 @@ import (
 type Session struct {
 	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	SessionToken string             `bson:"session_token" json:"session_token" validate:"required"`
-	Device       []string             `bson:"device" json:"device" validate:"required"`
+	Device       []string           `bson:"device" json:"device" validate:"required"`
 	UserID       primitive.ObjectID `bson:"user_id" json:"user_id" validate:"required"`
 	Expires      time.Time          `bson:"expires" json:"expires" validate:"required"`
 }
