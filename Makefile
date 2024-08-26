603c8a8d7f52b4098b9a71bac9fa434952a857f1
rcsen <rohansen856@gmail.com>
2024-08-26 06:48:12 +0530

    ui refactors

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ pull:
 	git pull origin main
 
 db:
-	docker run --name my-postgres -d -p 5432:5432 -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=password -e POSTGRES_DB=mydatabase postgres
+	docker run --name my-postgres -d -p 5432:5432 -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=password -e POSTGRES_DB=mydatabase postgres:16 & npx prisma db push
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -27,7 +27,7 @@ export default async function IndexPage() {
                   `animate-gradient inline bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
                 )}
               >
-                Explore all courses
+                Explore all benifits
               </span>
               <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
             </AnimatedGradientText>
@@ -35,63 +35,79 @@ export default async function IndexPage() {
         </div>
         <div className="container flex max-w-7xl flex-col items-center gap-4 text-center">
           <h1 className="font-heading text-2xl sm:text-4xl md:text-5xl lg:text-6xl">
-            Stand Out in Your College Applications With Live Project-Based
-            Bootcamps, Workshops & Mentorship
+            Breaking Barriers, Building Trust: Your Source for Decentralized
+            Journalism
           </h1>
           <p className="mt-6 flex gap-4 text-xl lg:gap-8 2xl:gap-12">
-            High schoolers, elevate your profile by engaging in hands-on
-            projects, conducting research, and receiving expert mentorship to
-            strengthen your college applications.
+            Unleashing the Power of Truth: Decentralized News for a Connected
+            World
           </p>
         </div>
 
         <div className="flex flex-wrap items-center justify-center gap-6 pt-12">
           <Link
-            href={"/programs#technology"}
+            href={"/dashboard"}
             className={buttonVariants({
-              className: "w-[400px] border-2 border-yellow-600 p-6",
+              className: "w-[300px] border-2 border-yellow-600 p-6",
               variant: "secondary",
             })}
           >
-            Technology Programs
+            Personalized News
           </Link>
           <Link
-            href={"/programs#entrepreneurship"}
+            href={"/news#trending"}
             className={buttonVariants({
-              className: "w-[400px] border-2 border-yellow-600 p-6",
+              className: "w-[300px] border-2 border-yellow-600 p-6",
               variant: "secondary",
             })}
           >
-            Entrepreneurship Programs
+            Top Trending News
           </Link>
           <Link
-            href={"/programs#research"}
+            href={"/news#research"}
             className={buttonVariants({
-              className: "w-[400px] border-2 border-yellow-600 p-6",
+              className: "w-[300px] border-2 border-yellow-600 p-6",
               variant: "secondary",
             })}
           >
-            Research Programs
+            Sports News
+          </Link>
+          <Link
+            href={"/news#upcoming"}
+            className={buttonVariants({
+              className: "w-[300px] border-2 border-yellow-600 p-6",
+              variant: "secondary",
+            })}
+          >
+            Financial News
           </Link>
         </div>
       </section>
       <section className="w-full bg-secondary py-8 md:py-12 lg:py-24">
         <h3 className="mb-12 text-center font-heading text-xl sm:text-2xl md:text-3xl lg:text-4xl">
-          PROGRAMS THAT DELIVER RESULTS
+          WHY WE&apos;RE OFFERING
         </h3>
         <div className="flex items-center justify-center gap-12">
           {[
-            { title: "Empowered", subtitle: "500+", value: "High Schoolers" },
-            { title: "Rated", subtitle: "4.5/5", value: "By Students" },
             {
-              title: "Led By",
-              subtitle: "20+",
-              value: "Selective Expert Mentors",
+              title: "Collection",
+              subtitle: "500+ ARTICLES",
+              value: "by trusted journalists",
             },
             {
-              title: "Helped Buid",
-              subtitle: "100+",
-              value: "Real World Projects",
+              title: "Fake news",
+              subtitle: "90% catch rate",
+              value: "By Our custom AI model",
+            },
+            {
+              title: "Authenticity",
+              subtitle: "Blockchain technology",
+              value: "for all of our news articles",
+            },
+            {
+              title: "AI/ML Integration",
+              subtitle: "5 unique AI features",
+              value: "Summary, Grammar checks etc.",
             },
           ].map((i) => (
             <div className="flex h-72 w-60 flex-col justify-between rounded-2xl bg-primary p-4 py-12 font-extrabold text-primary-foreground">
@@ -104,51 +120,51 @@ export default async function IndexPage() {
       </section>
       <section className="w-full bg-primary/90 p-12">
         <h3 className="mb-12 text-center font-heading text-xl text-primary-foreground sm:text-2xl md:text-3xl lg:text-4xl">
-          Trending Programs
+          Trending News
         </h3>
         <div className="flex flex-wrap items-center justify-center gap-24">
           {[
             {
-              title: "App Dev Internship Bootcamp Program",
-              desc: "The App Development Internship BootCamp by Fuse is an intensive program designed for high school students eager to dive into the world of app development. Over several weeks, participants learn the fundamentals of app development, work on hands-on projects, and receive mentorship from industry professionals. By the end of the bootcamp, students will have created their own fully functional apps, gaining practical experience, valuable resources, and networking opportunities. This experience not only equips them with essential tech skills but also adds significant value to their college applications, demonstrating creativity, problem-solving, and a proactive approach to learning.",
+              title: "50B  people dead in 2024",
+              desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime iusto sed, repellendus delectus, nostrum nihil fugiat laborum architecto, est eum doloremque inventore beatae distinctio eius error amet incidunt iure minima provident voluptatibus. Deleniti illo commodi amet. Suscipit possimus, aperiam maiores velit saepe animi, commodi sapiente error exercitationem nostrum magni voluptates!",
             },
             {
-              title: "AI/ML Internship Bootcamp program",
-              desc: "The AI/ML Internship BootCamp by Fuse is a focused program designed for high school students interested in Artificial Intelligence and Machine Learning. Over several weeks, participants learn core AI/ML concepts, engage in hands-on projects, and receive mentorship from industry experts. By the end of the bootcamp, students will have developed their own AI/ML models and gained practical experience, networking opportunities, and access to valuable resources. This experience not only builds essential tech skills but also adds significant value to college applications, showcasing a strong commitment to learning and innovation in a cutting-edge field.",
+              title: "10,000 Suicides in India",
+              desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quia saepe eligendi possimus nihil fuga optio ex aut sequi impedit, ipsa facere placeat odit, incidunt, harum accusantium velit ipsum sit dignissimos ut aspernatur rem soluta! Eligendi nihil commodi eos ex veniam modi voluptate dolorum quia aperiam repudiandae, explicabo alias. Dolore possimus laudantium, quasi libero aperiam incidunt laborum illo perspiciatis harum tempore doloribus, nulla molestiae dolorum maxime nam voluptas. Cumque, eaque!",
             },
           ].map((i) => (
             <div className="relative h-[500px] w-[450px] overflow-hidden rounded-3xl border bg-primary/70 pt-12 text-primary-foreground shadow-xl">
               <h3 className="mb-8 px-12 text-2xl">{i.title}</h3>
-              <p className="px-12">4 week internship program</p>
+              <p className="px-12">Trending since 2 days</p>
               <p className="mb-6 px-12">⭐⭐⭐⭐⭐</p>
               <p className="px-12 text-xs">{i.desc}</p>
               <Link
                 href={"/"}
                 className="absolute bottom-0 flex w-full items-center justify-center gap-6 border-t bg-secondary/30 p-3 text-center text-2xl"
               >
-                View Course
+                View Full Article
                 <ChevronRight />
               </Link>
             </div>
           ))}
         </div>
         <h3 className="mb-12 mt-24 text-center font-heading text-xl text-primary-foreground sm:text-2xl md:text-3xl lg:text-4xl">
-          Previous Programs
+          Recent Events
         </h3>
         <div className="flex flex-wrap items-center justify-center gap-24">
           {[
             {
-              title: "Computer Science and Business Internship Program",
-              desc: "The Computer Science and Business Internship BootCamp by Fuse is a comprehensive program tailored for high school students interested in the intersection of technology and business. Over several weeks, participants gain foundational knowledge in computer science, explore business concepts, and work on real-world projects that integrate both fields. Guided by industry mentors, students develop practical skills, create business solutions, and build a strong professional network. This experience not only enhances their technical and entrepreneurial abilities but also adds significant value to college applications, showcasing a well-rounded skill set and a forward-thinking approach to their education.",
+              title: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+              desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque placeat ipsum quibusdam nisi excepturi commodi, soluta facere esse tempora sed illum aliquid ut alias facilis laudantium eum rerum asperiores dolorum quis, eaque dolores aperiam minima? Vero veritatis pariatur modi voluptatum quam sequi consectetur quae cum repellendus eligendi! Voluptatum ducimus possimus delectus soluta voluptas tempore tempora odio, error maiores nemo nam corporis obcaecati iste in facilis commodi facere, accusamus fugit sit dolores veniam dicta quidem? Fugit maxime corrupti aperiam itaque nulla. Ipsa laudantium, similique corrupti esse ",
             },
             {
-              title: "STEM Hackathon to High School Students",
-              desc: "The STEM Hackathon by Fuse is an exciting, fast-paced event designed for high school students passionate about Science, Technology, Engineering, and Math. Over the course of a weekend, participants collaborate in teams to solve real-world challenges, using their STEM skills to innovate and create practical solutions. Guided by mentors and industry experts, students engage in coding, design, and problem-solving, culminating in a final presentation of their projects. This experience not only hones critical thinking and teamwork but also adds significant value to college applications, highlighting creativity, collaboration, and technical expertise in STEM fields",
+              title: "Lorem ipsum dolor sit amet consectetur, adipisicing",
+              desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt beatae quae, consequuntur hic maiores, ullam, tempore blanditiis quam reprehenderit dolores animi cupiditate molestiae officiis porro eos. Nisi tempore exercitationem similique nostrum quo quis illo? Libero consequuntur blanditiis sequi repudiandae voluptas, consequatur vel. Nostrum consectetur quibusdam laboriosam! Obcaecati voluptatem ea quaerat sequi delectus porro sed, praesentium omnis amet quasi eum aspernatur vel. Atque debitis, excepturi quod, nobis deserunt voluptates esse vero sapiente explicabo consectetur tempore dolores inventore corporis at tenetur. Error.",
             },
           ].map((i) => (
             <div className="relative h-[550px] w-[450px] overflow-hidden rounded-3xl border bg-primary/70 pt-12 text-primary-foreground shadow-xl">
               <h3 className="mb-8 px-12 text-2xl">{i.title}</h3>
-              <p className="px-12">4 week internship program</p>
+              <p className="px-12">Happened 10hr. ago</p>
               <p className="mb-2 px-12">⭐⭐⭐⭐⭐</p>
               <div className="mb-6 flex items-center gap-4 px-12">
                 <span className="size-12 rounded-full bg-green-700" />
@@ -159,7 +175,7 @@ export default async function IndexPage() {
                 href={"/"}
                 className="absolute bottom-0 flex w-full items-center justify-center gap-6 border-t bg-secondary/30 p-3 text-center text-2xl"
               >
-                View Course
+                View Full Article
                 <ChevronRight />
               </Link>
             </div>
@@ -168,14 +184,14 @@ export default async function IndexPage() {
       </section>
       <section className="w-full bg-primary/90 py-12">
         <h3 className="mb-12 mt-24 pl-36 font-heading text-xl text-primary-foreground sm:text-2xl md:text-3xl lg:text-4xl">
-          Bettermind advantage
+          Fuse News advantage
         </h3>
         <div className="flex bg-background p-36 pt-12">
           <div className="">
             <h4 className="mb-6 text-3xl">Learn from the experts</h4>
             <p className="max-w-lg text-xl">
-              Develop real-world skills with our programs and projects designed
-              by industry experts and practitioners
+              Develop real-world skills with our news and projects designed by
+              industry experts and practitioners
             </p>
           </div>
           <div className="flex"></div>
@@ -184,7 +200,7 @@ export default async function IndexPage() {
           <div className="">
             <h4 className="mb-6 text-3xl">Why Fuse?</h4>
             <p className="max-w-lg text-xl">
-              We focus on personalized mentorship and real-world application
+              We focus on personalized news feeds and real-world application
               with live projects to craft your X factor and help you uniquely
               stand out in your college applications
             </p>
@@ -194,15 +210,15 @@ export default async function IndexPage() {
         <div className="flex justify-center gap-16 bg-background p-36 pt-12">
           {[
             {
-              title: "Real Hands-on Projects",
+              title: "Real Hands-on reporters",
               desc: "Work on real world problems and solve using technology",
             },
             {
-              title: "Real Hands-on Projects",
+              title: "Real Hands-on reporters",
               desc: "Solve problems with expert guidance in the field",
             },
             {
-              title: "Real Hands-on Projects",
+              title: "Real Hands-on reporters",
               desc: "Receive signed letters of recommendation and certificate to showcase your contribution value",
             },
           ].map((i) => (
@@ -215,21 +231,21 @@ export default async function IndexPage() {
       </section>
       <section className="flex flex-col bg-primary text-primary-foreground">
         <h3 className="mb-6 text-center text-5xl font-bold">
-          Why highschoolers love BetterMind?
+          Why people love Fuse News?
         </h3>
         <p className="mb-12 text-center">
-          SEE WHAT HIGH SCHOOLERS HAVE TO SAY ABOUT OUR PROGRAMS
+          SEE WHAT HIGH READERS HAVE TO SAY ABOUT OUR ARTICLES
         </p>
         <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-around gap-12">
           {[
             {
               rating: "4.9 Rating",
-              desc: "Participating in the internship and mentorship program was a game-changer for me. The hands-on experience and mentorship I received helped me stand out in my college applications. I'm beyond excited to be joining Stanford University! The topics were  taught in a clear. I really enjoyed the lessons.",
+              desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dolorum commodi reiciendis iste nam itaque impedit odit provident placeat animi, fugiat, hic voluptas repudiandae? Beatae dolore quidem ipsam sint. Ad consequuntur, fugit dolor magni quia dignissimos quibusdam autem cupiditate quam.",
               id: "Riya Sharma Texas",
             },
             {
               rating: "4.9 Rating",
-              desc: "Participating in the internship and mentorship program was a game-changer for me. The hands-on experience and mentorship I received helped me stand out in my college applications. I'm beyond excited to be joining Stanford University! The topics were  taught in a clear. I really enjoyed the lessons.",
+              desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dolorum commodi reiciendis iste nam itaque impedit odit provident placeat animi, fugiat, hic voluptas repudiandae? Beatae dolore quidem ipsam sint. Ad consequuntur, fugit dolor magni quia dignissimos quibusdam autem cupiditate quam.",
               id: "Riya Sharma Texas",
             },
           ].map((i) => (
@@ -246,7 +262,7 @@ export default async function IndexPage() {
                   Still Confused?
                 </h2>
                 <h2 className="text-2xl font-extrabold">
-                  Need more information about the programs? Talk to an expert
+                  Need more information about the articles? Talk to an expert
                   =&gt;
                 </h2>
               </div>
@@ -263,16 +279,16 @@ export default async function IndexPage() {
       >
         <div className="flex h-[20vh] w-full flex-col">
           <h3 className="mx-auto mb-12 max-w-5xl text-center font-heading text-5xl text-primary-foreground">
-            Boost your college applications with our tech & research programs
+            Boost your general knowledge with our tech & research news
           </h3>
           <Link
             href={"/"}
             className={buttonVariants({
               className:
-                "rounded-[20px] bg-primary-foreground hover:bg-primary-foreground px-16 text-xl mx-auto text-white mb-36",
+                "rounded-[20px] bg-primary-foreground px-16 text-xl mx-auto text-white mb-36 hover:bg-secondary",
             })}
           >
-            Explore Programs
+            Explore All Articles
           </Link>
         </div>
       </section>
